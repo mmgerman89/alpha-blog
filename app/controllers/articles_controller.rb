ca76db0cf79a99de1a9e451319bc8620bf8c220a
Germán Martínez <mmgerman89@gmail.com>
2017-12-05 13:34:32 -0300

    admin user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user && !current_user.admin?
         flash[:danger] = "Solo puedes editar o eliminar tus propios artículos"
         redirect_to root_path
       end
