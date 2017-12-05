2f296fc08413a7b59eb6997ea6fc8092f53d0ce8
Germán Martínez <mmgerman89@gmail.com>
2017-12-05 13:10:14 -0300

    complete authenticate system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Bienvenido"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Hubo un problema con el inicio de sesión"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "Hasta pronto."
+    redirect_to root_path
   end
 end
