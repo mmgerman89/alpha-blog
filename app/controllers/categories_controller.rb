ee3bbbe270660193925249cb26de594c7a49d39d
Germán Martínez <mmgerman89@gmail.com>
2017-12-10 00:55:29 -0300

    before action category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :show]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -20,11 +21,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:success] = "Categoría actualizada."
       redirect_to category_path(@category)
@@ -34,7 +33,6 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -48,5 +46,9 @@ class CategoriesController < ApplicationController
         flash[:danger] = "Solo un administrador puede hacer esto."
         redirect_to categories_path
       end
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
     end
 end
